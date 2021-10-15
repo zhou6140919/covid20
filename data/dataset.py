a2a6d77a0ac748ffb21dbbef787028fe99be753c
zhoutong <1534643901@qq.com>
2021-10-15 14:51:45 +0800

    first edition

---
+++ data/dataset.py
@@ -0,0 +1,26 @@
+import os
+
+from PIL import Image
+from torch.utils import data
+
+
+class Xray(data.Dataset):
+    def __init__(self, root, transforms):
+        self.transforms = transforms
+        p_root = root + '/positive/'
+        n_root = root + '/negative/'
+        imgs_p = [os.path.join(p_root, img) for img in os.listdir(p_root)]
+        imgs_n = [os.path.join(n_root, img) for img in os.listdir(n_root)]
+        imgs = imgs_p + imgs_n
+        self.imgs = imgs
+
+    def __getitem__(self, index):
+        img_path = self.imgs[index]
+        label = 1 if 'positive' in img_path.split('/') else 0
+        data = Image.open(img_path)
+        data = data.convert('RGB')
+        data = self.transforms(data)
+        return data, label
+
+    def __len__(self):
+        return len(self.imgs)
---
+++ test.py
@@ -0,0 +1,31 @@
+import os
+
+import torch
+from torchvision import datasets, transforms
+batch_size = 4
+data_dir = './data'
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+# print(image_datasets)
+dataloaders_dict = {
+    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=0) for x in
+    ['train', 'val']}
+for inputs, labels in dataloaders_dict['train']:
+    print(inputs)
+    print('-' * 100)
+    print(labels)
+    break
